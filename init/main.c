8f2e2b3e4076c0e00cb80101130c67460bd2b792
user_why_red <san8754513543@gmail.com>
2023-10-31 06:57:56 +0000

    init:Provide arch_cpu_finalize_init()

--- init/main.c
+++ init/main.c
@@ -786,9 +786,11 @@ asmlinkage __visible void __init start_kernel(void)
 	cgroup_init();
 	taskstats_init_early();
 	delayacct_init();
-
+	arch_cpu_finalize_init();
+	/* Temporary conditional until everything has been converted */
+#ifndef CONFIG_ARCH_HAS_CPU_FINALIZE_INIT
 	check_bugs();
-
+#endif
 	acpi_subsystem_init();
 	arch_post_acpi_subsys_init();
 	sfi_init_late();
